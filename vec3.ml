b060000847a43c46c4410a96d45dc5e495de3dfe
mrkuc <2574472+mrkuc@users.noreply.github.com>
2019-01-28 08:38:56 +0900

    add functions to vec3

--- vec3.ml
+++ vec3.ml
@@ -12,6 +12,12 @@ module Vec3 = struct
   let xyz v = (v.x, v.y, v.z)
 
   let squared_length v = v.x *. v.x +. v.y *. v.y +. v.z *. v.z
-  let length v = sqrt @@ squared_length v
+
+  let length v = Float.sqrt @@ squared_length v
+
+  let make_unit_vector v =
+    let k = 1.0 /. (Float.sqrt (v.x *. v.x +. v.y *. v.y +. v.z *. v.z))
+    in create (v.x *. k) (v.y *. k) (v.z *. k)
+    
 end
 
