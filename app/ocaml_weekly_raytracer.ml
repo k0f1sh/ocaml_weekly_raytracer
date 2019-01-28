90913d796cccf1e758ab89a09f28528acb5676d3
mrkuc <2574472+mrkuc@users.noreply.github.com>
2019-01-29 03:52:24 +0900

    background

--- app/ocaml_weekly_raytracer.ml
+++ app/ocaml_weekly_raytracer.ml
@@ -1,19 +1,35 @@
 open Base
 module Out = Stdio.Out_channel
 module Vec3 = Raylib.Vec3
+module Ray = Raylib.Ray
 
 let nx = 200
 let ny = 100
 let output_file_name = "output.ppm"
 
+let color r =
+  let unit_direction = Vec3.unit_vector (Ray.direction r) in
+  let t = 0.5 *. ((Vec3.y unit_direction) +. 1.0) in
+  let a = 1.0 -. t in
+  let open Vec3 in
+  ((Vec3.create 1.0 1.0 1.0) *. a) + ((Vec3.create 0.5 0.7 1.0) *. t)
+
+let get_ray origin lower_left_corner horizontal vertical u v =
+  let open Vec3 in
+  Ray.create origin (lower_left_corner + (horizontal *. u) + (vertical *. v))
+
 (* 1ピクセル分を描画する *)
 (* Out_channel.t -> int -> int -> unit *)
 let render chan x y =
-  let x = (Int.to_float x) /. (Int.to_float nx) in
-  let y = (Int.to_float y) /. (Int.to_float ny) in
-  let z = 0.2 in
-  let v = Vec3.create x y z in
-  let (r, g, b) = Vec3.xyz v in
+  let lower_left_corner = Vec3.create (-2.0) (-1.0) (-1.0) in
+  let horizontal = Vec3.create 4.0 0.0 0.0 in
+  let vertical = Vec3.create 0.0 2.0 0.0 in
+  let origin = Vec3.create 0.0 0.0 0.0 in
+  let u = (Int.to_float x) /. (Int.to_float nx) in
+  let v = (Int.to_float y) /. (Int.to_float ny) in
+  let r = get_ray origin lower_left_corner horizontal vertical u v in
+  let col = color r in
+  let (r, g, b) = Vec3.xyz col in
   begin
     Out.output_string
       chan
