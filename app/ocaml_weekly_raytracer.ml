e4af914dadaf754e68ca432b4133ecfd089b525d
mrkuc <2574472+mrkuc@users.noreply.github.com>
2019-02-01 01:28:58 +0900

    Render diffuse reflection material

--- app/ocaml_weekly_raytracer.ml
+++ app/ocaml_weekly_raytracer.ml
@@ -11,10 +11,24 @@ let ny = 100
 let ns = 100
 let output_file_name = "output.ppm"
 
-let color r hitable =
+let rec random_in_unit_sphere () =
+  let p = (Vec3.minus
+             (Vec3.mulf
+                (Vec3.create (Random.float 1.0) (Random.float 1.0) (Random.float 1.0))
+                2.0)
+             (Vec3.create 1.0 1.0 1.0)) in
+  if Caml.(>=) (Vec3.squared_length p) 1.0 then
+    random_in_unit_sphere ()
+  else
+    p
+  
+let rec color r hitable =
   match (Hitable.hit hitable r 0.0 Float.max_finite_value) with
     Some hit_record ->
-     Vec3.mulf (Vec3.plus (Vec3.create 1.0 1.0 1.0) (Hit_record.normal hit_record)) 0.5
+     let target = (Vec3.plus (Vec3.plus (Hit_record.p hit_record)
+                                        (Hit_record.normal hit_record))
+                             (random_in_unit_sphere ())) in
+     Vec3.mulf (color (Ray.create (Hit_record.p hit_record) (Vec3.minus target (Hit_record.p hit_record))) hitable) 0.5
   | None ->
      let unit_direction = Vec3.unit_vector (Ray.direction r) in
      let t = 0.5 *. ((Vec3.y unit_direction) +. 1.0) in
@@ -39,7 +53,9 @@ let sample_ns x y =
               let u = ((Int.to_float x) +. Random.float 1.0) /. (Int.to_float nx) in
               let v = ((Int.to_float y) +. Random.float 1.0) /. (Int.to_float ny) in
               Vec3.plus col @@ sample u v) in
-  Vec3.divf col @@ Int.to_float ns
+  let col = Vec3.divf col @@ Int.to_float ns in
+  let (x, y, z) = Vec3.xyz col in
+  Vec3.create (Float.sqrt x) (Float.sqrt y) (Float.sqrt z)
 
 (* 1ピクセル分を描画する *)
 (* Out_channel.t -> int -> int -> unit *)
