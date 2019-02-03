60dae1ef193da680cbac942c49322898e91d9234
mrkuc <2574472+mrkuc@users.noreply.github.com>
2019-02-03 16:53:48 +0900

    fov

--- lib/camera.ml
+++ lib/camera.ml
@@ -1,20 +1,24 @@
-type t = { origin: Vec3.t;
-           lower_left_corner: Vec3.t;
-           horizontal: Vec3.t;
-           vertical: Vec3.t
-         }
+open Base
 
-let create origin lower_left_corner horizontal vertical = { origin;
-                                                            lower_left_corner;
-                                                            horizontal;
-                                                            vertical
-                                                          }
+type t = {
+    origin: Vec3.t;
+    lower_left_corner: Vec3.t;
+    horizontal: Vec3.t;
+    vertical: Vec3.t
+  }
 
-let default_camera = create
-                       (Vec3.create 0.0 0.0 0.0)
-                       (Vec3.create (-2.0) (-1.0) (-1.0))
-                       (Vec3.create 4.0 0.0 0.0)
-                       (Vec3.create 0.0 2.0 0.0)
+let create vfov aspect =
+  let theta = vfov *. Float.pi /. 180.0 in
+  let half_height = Float.tan @@ theta /. 2.0 in
+  let half_width = aspect *. half_height in
+  {
+    origin = Vec3.create 0.0 0.0 0.0;
+    lower_left_corner = Vec3.create ((-1.0) *. half_width) ((-1.0) *. half_height) (-1.0);
+    horizontal = Vec3.create (2.0 *. half_width) 0.0 0.0;
+    vertical = Vec3.create 0.0 (2.0 *. half_height) 0.0;
+  }
+
+let default_camera = create 120.0 2.0
 
 let get_ray camera u v =
   Ray.create camera.origin (Vec3.minus (Vec3.plus
--- lib/camera.mli
+++ lib/camera.mli
@@ -1,7 +1,7 @@
 open Base
 
 type t
-val create: Vec3.t -> Vec3.t -> Vec3.t -> Vec3.t -> t
+val create: float -> float -> t
 val default_camera: t
 val get_ray: t -> float -> float -> Ray.t
 
